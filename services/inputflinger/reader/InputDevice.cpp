67d840903b608170d99dfb7b465a9028d68e56a4
Pierre-Hugues Husson <phh@phh.me>
2020-10-22 23:22:46 +0200

    Matching an input with a display uses uniqueId
    Not all devices have a `location`, notably bluetooth devices.
    However, we might still want to associate them with a screen,
    so match them with uniqueId indeed.
    
    This is useful to have bluetooth keyboard in desktop mode for instance.
    
    Change-Id: Ifcbc8329d54386f58e013270d9888316c0f516b6

--- services/inputflinger/reader/InputDevice.cpp
+++ services/inputflinger/reader/InputDevice.cpp
@@ -264,7 +264,10 @@ std::list<NotifyArgs> InputDevice::configure(nsecs_t when,
             mAssociatedDisplayUniqueId = std::nullopt;
             mAssociatedViewport = std::nullopt;
             // Find the display port that corresponds to the current input port.
-            const std::string& inputPort = mIdentifier.location;
+            std::string inputPort = mIdentifier.location;
+            if (inputPort.empty()) {
+                inputPort = mIdentifier.uniqueId;
+            }
             if (!inputPort.empty()) {
                 const std::unordered_map<std::string, uint8_t>& ports =
                         readerConfig.portAssociations;
