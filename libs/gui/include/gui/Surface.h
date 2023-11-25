6bde488aa8ac93ce52ad347b806ee8f004dda32d
Pierre-Hugues Husson <phh@phh.me>
2023-11-25 08:15:28 -0500

    Add MTK GED KPI support to fix broken Mediatek gpufreq
    Mediatek GPU scheduler likes to have the timestamps of the frames to be
    able to adjust DVFS.
    Technically this isn't completely needed, because it looks like DVFS can
    work once we started triggering /proc/ged. But now that the work is
    done, let's do it completely.
    
    In benchmarks on Poco X4 GT, before this patch result is 1500, after is
    5700. If we disable the patch after enabling it without rebooting, it
    goes to 5400. So looks like GED KPI thingy does try to do a bit more
    than just standard DVFS.
    
    Thanks @sarthakroy2002 for the help and support (other people helped as
    well)

--- libs/gui/include/gui/Surface.h
+++ libs/gui/include/gui/Surface.h
@@ -638,6 +638,9 @@ protected:
 
     // Buffers that are successfully dequeued/attached and handed to clients
     std::unordered_set<int> mDequeuedSlots;
+
+    pid_t mPid;
+    bool mIsSurfaceFlinger;
 };
 
 } // namespace android
