0305a56ab33693e230926e4495b34c292961fd71
Pierre-Hugues Husson <phh@phh.me>
2024-02-03 07:38:33 -0500

    Increase SW_MAX *= 4
    We increase the size of SW_MAX for two reasons:
    1. SW_MACHINE_COVER which is SW_MAX is ignored, guessing an off by one
       somwehere
    2. Samsung extends SW_ beyond standard SW_MAX. SW_FLIP goes to 0x15,
       standard SW_MAX 0x10

--- services/inputflinger/reader/EventHub.cpp
+++ services/inputflinger/reader/EventHub.cpp
@@ -1142,7 +1142,7 @@ int32_t EventHub::getKeyCodeForKeyLocation(int32_t deviceId, int32_t locationKey
 }
 
 int32_t EventHub::getSwitchState(int32_t deviceId, int32_t sw) const {
-    if (sw < 0 || sw > SW_MAX) {
+    if (sw < 0 || sw > (SW_MAX*4)) {
         return AKEY_STATE_UNKNOWN;
     }
     std::scoped_lock _l(mLock);
@@ -2500,7 +2500,7 @@ void EventHub::openDeviceLocked(const std::string& devicePath) {
     }
 
     // Check whether this device has switches.
-    for (int i = 0; i <= SW_MAX; i++) {
+    for (int i = 0; i <= (SW_MAX * 4); i++) {
         if (device->swBitmask.test(i)) {
             device->classes |= InputDeviceClass::SWITCH;
             break;
