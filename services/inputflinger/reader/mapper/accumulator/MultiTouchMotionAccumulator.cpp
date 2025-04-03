085e596fcb18378f46e857d0efa8a7fdbbf958ea
WUKAWL <21525471@qq.com>
2025-04-03 22:00:26 +0800

    Fix touch on MEIZU 21 Pro (#1)
    * Fix touch on Meizu 21 Pro
    
    Raw values from the panel are multiplied by 10,
    causing most touch events to fall out of bounds and get dropped
    
    * Fix touch on Meizu 21 Pro
    
    Raw values from the panel are multiplied by 10, causing most touch events to fall out of bounds and get dropped

--- services/inputflinger/reader/mapper/accumulator/MultiTouchMotionAccumulator.cpp
+++ services/inputflinger/reader/mapper/accumulator/MultiTouchMotionAccumulator.cpp
@@ -153,7 +153,7 @@ void MultiTouchMotionAccumulator::populateCurrentSlot(
 
 MultiTouchMotionAccumulator::Slot::Slot() {
     std::string targetDevice = android::base::GetProperty("ro.product.vendor.device", "");
-    if (targetDevice == "meizu21" || targetDevice == "Meizu21Note") {
+    if (targetDevice == "meizu21" || targetDevice == "meizu21Pro" || targetDevice == "Meizu21Note") {
         mAbsMtPositionXYRatio = 10;
     }
 }
